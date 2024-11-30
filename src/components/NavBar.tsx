1cdb04ef983b00f73078b1be01464abd1ea8fb0f
campbell-frost <campbellsfrost@gmail.com>
2024-11-30 16:56:35 -0500

    front end

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import ToggleTheme from "./ToggleTheme";
-
+import { Button } from "./ui/Button";
+import { Menu } from "lucide-react";
 export default function NavBar() {
   return (
     <div className="flex w-full justify-between items-center h-16 border-b border-border">
@@ -10,8 +11,13 @@ export default function NavBar() {
             <h1 className="text-2xl font-bold">Policy Link Solutions</h1>
           </Link>
         </h1>
-        <div className="flex items-center gap-4">
+        <div className="items-center gap-4 hidden sm:flex">
           <ToggleTheme />
+        </div>
+        <div className="sm:hidden flex items-center gap-4">
+          <Button variant="ghost" size="icon">
+            <Menu />
+          </Button>
         </div>
       </div>
     </div>
--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import { Button } from './ui/Button';
 
 export default function ToggleTheme() {
   const { setTheme, resolvedTheme } = useTheme();
@@ -13,9 +14,9 @@ export default function ToggleTheme() {
   };
 
   return (
-    <button onClick={handleThemeChange} className="flex hover:bg-accent p-2 rounded-md">
+    <Button onClick={handleThemeChange} variant="ghost" size="icon">
       <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-180 dark:scale-0" />
       <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    </button>
+    </Button>
   );
 }
